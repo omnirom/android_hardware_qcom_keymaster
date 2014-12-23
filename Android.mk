3f0d62e1b0a76b664e518c5b7cb7b1e2be52deb0
maxwen <max.weninger@windriver.com>
2014-12-23 14:33:50 +0100

    keymaster: old ion heap mask must be used for all non AOSP QCOM devices
    This fixes wrongly changed https://gerrit.omnirom.org/#/c/10998/
    
    Change-Id: I8422da16b2ff658bbd7442c8a2895a19543ea829

--- Android.mk
+++ Android.mk
@@ -3,7 +3,11 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter msm8960 msm8974 msm8226 msm8084,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
-ifneq ($(filter msm8084 msm8974,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8084)
+keymaster-def += -D_ION_HEAP_MASK_COMPATIBILITY_WA
+endif
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 keymaster-def += -D_ION_HEAP_MASK_COMPATIBILITY_WA
 endif
 
